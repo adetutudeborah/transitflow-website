d3748be8c650df86eab750fdc24eec09c4afba1a
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-03-29 00:31:42 +0100

    added the header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
 import './Header.css'; 
+import images from "../../assets/image";
 
 function Header() { 
     return(
      <div className='container'>    
-         Header
+        <div className='headercontainer'>
+
+            <div className='logo'>
+               <img src={images.logo} alt="logo" className='logo'/>
+            </div>
+            
+
+            <div className='headerinnercontainer'>
+
+                <div className='headerinfo'>
+                    <img src={images.time} alt="time-icon" className=''/>  
+
+                    <div className='headertext'>
+                        <p>Mon - Sat 9.00 - 18.00</p> 
+                        <p>Sunday Closed</p>
+                    </div>
+                    
+                </div>
+
+                <div className='headerinfo'>
+                    <img src={images.email} alt="email-icon" className=''/>  
+
+                    <div className='headertext'>
+                        <p>Email</p> 
+                        <p>contact@logistics.com</p>
+                    </div>
+
+                    
+                </div>
+
+                <div className='headerinfo'>
+                    <img src={images.call} alt="call-icon" className=''/>  
+
+                    <div className='headertext'>
+                        <p>Call Us</p> 
+                        <p>(00) 112 365 489 </p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
     </div>    
     )
 }
--- src/assets/image.js
+++ src/assets/image.js
@@ -1,9 +1,13 @@
-import logo from "./react.svg"; 
-
+import logo from './header/logo.png'
+import time from './header/time-icon.png'
+import email from './header/email-icon.png'
+import call from './header/call-icon.png'
 
 const images = {
   logo,
-
+  time,
+  email,
+  call,
 };
 
 export default images;
