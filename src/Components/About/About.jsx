9c42b6429624188f28ab143affc6e4abf0784c3b
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-03-29 15:55:34 +0100

    added the about component

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,12 +1,58 @@
 import React from 'react';
 import './About.css'; 
+import images from "../../assets/image";
 
 function About() { 
     return(
-     <div className='container'>    
-         About
+     <div className='aboutsection'>    
+         <img src={images.aboutbackground} alt="about-backgroung-image" className='aboutbackgroundimg'/>
+
+        <div className='aboutcontainer'>
+
+            <div className='aboutinnercontainer'>
+
+                <div className='aboutcontent'>
+                        <span className='aboutcontenttext'>Why Us</span>
+                        <h2 className='aboutcontentmaintext'>We provide full range global <br /> logistics solution</h2>
+                        <p className='aboutcontentsubtext'>
+                        Leverage agile frameworks to provide a robust synopsis for <br /> strategy foster collaborative thinking to further the overall <br /> value proposition. 
+                        <br />
+                        <br />
+                        Organically grow the holistic world view of disruptive <br /> innovation via workplace diversity and empowerment.
+                        </p>
+
+                        <div className='aboutfeature'>
+                            <img src={images.deliveryicon} alt="delivery-time-icon" className='featureicon'/>
+                            <p className='featuretext'>Delivery on Time</p>
+                        </div>
+
+                        <div className='aboutfeature'>
+                            <img src={images.costicon} alt="travel-cost-icon" className='featureicon'/>
+                            <p className='featuretext'>Optimized Travel Cost</p>
+                        </div>
+                </div>
+
+                <div className='aboutimage'>
+                        <img src={images.aboutimage} alt="about-image" className=''/>
+                </div>
+            </div>
+        </div>
+
+        <div className='aboutcounter'>
+                <div className='counter counterleft'>
+                    <span className='number'>1294</span>
+                    <span> <img src={images.countericon} alt="countericon" className='countericon' /> </span>
+                    <span className='countercontent'>Delivered Packages</span>
+                </div>
+
+                <div className='counter'>
+                    <span className='number'>3594</span>
+                    <span> <img src={images.countericon} alt="countericon" className='countericon' /> </span>
+                    <span className='countercontent'>Satisfied</span>
+                </div>
+        </div>
     </div>    
     )
 }
 
-export default About;
+export default About; 
--- src/assets/image.js
+++ src/assets/image.js
@@ -6,6 +6,11 @@ import seatransport from './services/sea-transport-icon.png';
 import warehousing from './services/warehousing-icon.png';
 import airfright from './services/air-fright-icon.png';
 import localshipping from './services/local-shipping-icon.png';
+import aboutbackground from './about/about-background.png';
+import deliveryicon from './about/delivery-time-icon.png';
+import costicon from './about/travel-cost-icon.png';
+import aboutimage from './about/about-image.png';
+import countericon from './about/counter-icon.png'
 
 const images = {
   logo,
@@ -16,9 +21,14 @@ const images = {
   warehousing,
   airfright,
   localshipping,
+  aboutbackground,
+  deliveryicon, 
+  costicon,
+  aboutimage,
+  countericon
 };
 
 export default images;
 
-// import images from "../../assets/images";
+// import images from "../../assets/image";
 // To import the images in other folders
